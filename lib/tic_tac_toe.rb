39cb7774c9f7951ab687ceffe379a0bf39eeb6fe
Joann Pan <joannlpan@gmail.com>
2020-04-29 22:16:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ def current_player(board)
 end
 
 def won?(board)
- WIN_COMBINATIONS.select do |row|
+ WIN_COMBINATIONS.detect do |row|
    board[row[0]] == board[row[1]] &&
    board[row[1]] == board[row[2]] &&
    position_taken?(board[row[0]])
