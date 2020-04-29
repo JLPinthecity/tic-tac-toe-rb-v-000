70607b81417541fc2b9847cf38b831b28091038b
Joann Pan <joannlpan@gmail.com>
2020-04-29 22:21:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,16 @@ end
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
     board[combo[0]] == board[combo[1]] &&
-      board[combo[1]] == board[combo[2]] &&
-      position_taken?(board, combo[0])
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
+end
+
+def full?(board)
+    board.all do |token| 
+      token = "X" || token = "O"
+    end
+     
+  end
+
 end
