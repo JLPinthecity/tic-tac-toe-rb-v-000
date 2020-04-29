292525bb73e3b71300e6152621673848bab82358
Joann Pan <joannlpan@gmail.com>
2020-04-29 22:01:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,8 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
     if valid_move?(board, index)
-      move(board, index, token)
+      move(board, index)
+      display_board(board)
     else
       turn
     end
